5cb331a3a1ac8b92029b3722ee16670a73c1c00f
Alex Athorne <a.thornysort@gmail.com>
2024-01-21 23:44:41 +0000

    Fix boundary collision physics

--- src/pyphyds/laws.py
+++ src/pyphyds/laws.py
@@ -32,18 +32,26 @@ class BoxBoundaryLaw(UniversalLaw):
         self.bounds = (self.x_min, self.x_max, self.y_min, self.y_max)
 
     def step(self, dt: float):
-        is_oob = self.particles.x[:, 0] < self.x_min
         # X-axis
+        is_oob = self.particles.x[:, 0] < self.x_min
         if is_oob.any():
             d_oob = self.particles.x[is_oob][:, 0] - self.x_min
             d_oob = torch.stack((d_oob, torch.zeros_like(d_oob))).T
             self.particles.x[is_oob] -= 2 * d_oob
+
+            d_oob = self.particles.old_x[is_oob][:, 0] - self.x_min
+            d_oob = torch.stack((d_oob, torch.zeros_like(d_oob))).T
+            self.particles.old_x[is_oob] -= 2 * d_oob
 
         is_oob = self.particles.x[:, 0] > self.x_max
         if is_oob.any():
             d_oob = self.particles.x[is_oob][:, 0] - self.x_max
             d_oob = torch.stack((d_oob, torch.zeros_like(d_oob))).T
             self.particles.x[is_oob] -= 2 * d_oob
+
+            d_oob = self.particles.old_x[is_oob][:, 0] - self.x_max
+            d_oob = torch.stack((d_oob, torch.zeros_like(d_oob))).T
+            self.particles.old_x[is_oob] -= 2 * d_oob
 
         # Y-axis
         is_oob = self.particles.x[:, 1] < self.y_min
@@ -52,8 +60,16 @@ class BoxBoundaryLaw(UniversalLaw):
             d_oob = torch.stack((torch.zeros_like(d_oob), d_oob)).T
             self.particles.x[is_oob] -= 2 * d_oob
 
+            d_oob = self.particles.old_x[is_oob][:, 1] - self.y_min
+            d_oob = torch.stack((torch.zeros_like(d_oob), d_oob)).T
+            self.particles.old_x[is_oob] -= 2 * d_oob
+
         is_oob = self.particles.x[:, 1] > self.y_max
         if is_oob.any():
             d_oob = self.particles.x[is_oob][:, 1] - self.y_max
             d_oob = torch.stack((torch.zeros_like(d_oob), d_oob)).T
             self.particles.x[is_oob] -= 2 * d_oob
+
+            d_oob = self.particles.old_x[is_oob][:, 1] - self.y_max
+            d_oob = torch.stack((torch.zeros_like(d_oob), d_oob)).T
+            self.particles.old_x[is_oob] -= 2 * d_oob
