30a8a0af08a4d348a65773e804d1925d14a623d7
Alex Athorne <a.thornysort@gmail.com>
2024-01-22 00:48:28 +0000

    Add two particle gas example

---
+++ src/examples/two_particles_gas.py
@@ -0,0 +1,65 @@
+from pyphyds.laws.box_boundary import BoxBoundaryLaw
+from pyphyds.interactions.collision_law import CollisionLaw
+from pyphyds.particles import Particles
+from pyphyds.simulation import Simulation
+import torch
+# Example file showing a basic pygame "game loop"
+import pygame
+
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+# simulation setup
+particles_1 = Particles(4, properties={'size': 25, 'color': "black"})
+particles_2 = Particles(100, properties={'size': 5, 'color': "red"})
+boundary_law_1 = BoxBoundaryLaw(
+    particles_1, 0, screen.get_width(), 0, screen.get_height(),
+)
+boundary_law_2 = BoxBoundaryLaw(
+    particles_2, 0, screen.get_width(), 0, screen.get_height(),
+)
+collision_law_11 = CollisionLaw([particles_1, particles_1])
+collision_law_12 = CollisionLaw([particles_1, particles_2])
+collision_law_22 = CollisionLaw([particles_2, particles_2])
+sim = Simulation(
+    particles=[particles_1, particles_2],
+    laws=[boundary_law_1, boundary_law_2],
+    interactions=[collision_law_11, collision_law_12, collision_law_22]
+)
+
+def draw_particles(particles):
+    for particle in particles.x:
+        particle_pos = pygame.Vector2(particle[0], particle[1])
+        pygame.draw.circle(
+            screen,
+            particles.properties['color'],
+            particle_pos,
+            particles.properties['size']
+        )
+
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("white")
+
+    # run simulation
+    sim.step(0.1)
+    draw_particles(particles_1)
+    draw_particles(particles_2)
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
--- src/pyphyds/interactions/collision_law.py
+++ src/pyphyds/interactions/collision_law.py
@@ -12,6 +12,7 @@ class CollisionLaw(InteractionLaw):
         assert len(particles) == 2, "Collision interaction law requires exactly two sets of particles"
         for particle in particles:
             assert 'size' in particle.properties
+        self.self_interaction = True if particles[0] is particles[1] else False
         self.p1w = particles[0].properties['size']
         self.p2w = particles[1].properties['size']
         super().__init__(particles, "Collision")
@@ -20,7 +21,8 @@ class CollisionLaw(InteractionLaw):
         p1, p2 = self.particles
         vect_diffs = p1.x[None, :] - p2.x[:, None]
         D = vect_diffs.norm(dim=-1)
-        D = D + torch.eye(D.shape[0]) * (self.p1w + self.p2w) + 0.01
+        if self.self_interaction:
+            D = D + torch.eye(D.shape[0]) * (self.p1w + self.p2w) + 0.01
         diff = D - (self.p1w + self.p2w)
         diff[diff > 0] = 0
         diff[diff < 0] = 1/2
