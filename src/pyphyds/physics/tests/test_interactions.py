f2383ff9318f35d5743c3e60f5c8670931a04ca7
Alex Athorne <a.thornysort@gmail.com>
2024-01-31 13:01:05 -0800

    Fix tests

--- src/pyphyds/physics/tests/test_interactions.py
+++ src/pyphyds/physics/tests/test_interactions.py
@@ -6,8 +6,8 @@ from pyphyds.physics.interactions.collision_interaction import CollisionInteract
 
 
 def test_particles():
-    p = Particles(2, x_bound=np.array([10, 10]), v_bound=np.array([3, 3]))
-    pm = ParticleMap(p, [0, 1], [0.5, 0.5])
-    p.x = torch.tensor([[5, 5], [5, 5]])
-    p.v = torch.tensor([[1, -1], [-1, 0]])
+    p = Particles(2, x_bound=np.array([10., 10.]), v_bound=1)
+    pm = ParticleMap(p, [0, 1], [0.5, 0.5], properties={0: {'size': 1}, 1: {'size': 1}})
+    p.x = torch.tensor([[5., 5.], [5., 5.]])
+    p.v = torch.tensor([[1., -1.], [-1., 0.]])
     CollisionInteraction(key_a=0, key_b=1, particle_map=pm)(0, 1)
